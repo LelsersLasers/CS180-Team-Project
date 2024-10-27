02c2b24f2320c69a40d25537fb24aec540f91b33
Millan Kumar <millankumar@gmail.com>
2024-10-26 22:15:53 -0400

    Planning to use object files instead of text files

--- backend/DatabaseInterface.java
+++ backend/DatabaseInterface.java
@@ -1,13 +1,13 @@
 package backend;
 
 public interface DatabaseInterface {
-	public final static String USERS_FILE = "database/users.txt";
-	public final static String POSTS_FILE = "database/posts.txt";
-	public final static String COMMENTS_FILE = "database/comments.txt";
-	public final static String FRIENDSHIP_FILE = "database/friendship.txt";
-	public final static String BLOCKED_FILE = "database/blocked.txt";
-	public final static String POST_VOTE_FILE = "database/post_vote.txt";
-	public final static String COMMENT_VOTE_FILE = "database/comment_vote.txt";
+	public final static String USERS_FILE = "database/users";
+	public final static String POSTS_FILE = "database/posts";
+	public final static String COMMENTS_FILE = "database/comments";
+	public final static String FRIENDSHIP_FILE = "database/friendship";
+	public final static String BLOCKED_FILE = "database/blocked";
+	public final static String POST_VOTE_FILE = "database/post_vote";
+	public final static String COMMENT_VOTE_FILE = "database/comment_vote";
 	
 
 	public void saveUser(User user);
